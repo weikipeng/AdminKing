030f6dba6f80c3ba2ee4e50000252752e713a725
wiki <wiki.peng@qq.com>
2017-04-11 20:00:14 +0800

    添加登录界面

--- app/src/main/java/com/pengjunwei/adminking/ui/LoginActivity.java
+++ app/src/main/java/com/pengjunwei/adminking/ui/LoginActivity.java
@@ -1,20 +1,19 @@
 package com.pengjunwei.adminking.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.pengjunwei.adminking.R;
 import com.pengjunwei.support.base.BaseActivity;
 
 /**
- * Created by WikiPeng on 2017/3/27 18:11.
+ * Created by WikiPeng on 2017/4/11 17:16.
  */
-public class SplashActivity extends BaseActivity{
+public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, LoginActivity.class));
-        finish();
+        setContentView(R.layout.layout_login);
     }
 }
