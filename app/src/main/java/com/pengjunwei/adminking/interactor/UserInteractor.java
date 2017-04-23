d61c549c179360a2bd3b6825fed7162d42490b80
wikipeng <pengjunwei@100tal.com>
2017-04-23 08:37:44 +0800

    #1.0#添加md5操作验证

--- app/src/main/java/com/pengjunwei/adminking/interactor/UserInteractor.java
+++ app/src/main/java/com/pengjunwei/adminking/interactor/UserInteractor.java
@@ -1,8 +1,14 @@
 package com.pengjunwei.adminking.interactor;
 
+import android.util.Base64;
+
 import com.pengjunwei.adminking.base.BaseInteractor;
 import com.pengjunwei.adminking.pojo.SLoginResult;
+import com.pengjunwei.support.tool.DateTool;
+import com.pengjunwei.support.tool.MD5;
 import com.pengjunwei.support.tool.RxSchedulersHelper;
+
+import java.io.UnsupportedEncodingException;
 
 import io.reactivex.Observable;
 import retrofit2.http.Field;
@@ -12,13 +18,12 @@ import retrofit2.http.POST;
 /**
  * Created by WikiPeng on 2017/4/11 20:05.
  */
-public class UserInteractor extends BaseInteractor{
+public class UserInteractor extends BaseInteractor {
     private interface WebInterface {
         /***/
         @POST("/kingmath/user/login.php")
         @FormUrlEncoded
-        Observable<SLoginResult> login(@Field("userName") String userName
-                , @Field("password") String password, @Field("s") String sign);
+        Observable<SLoginResult> login(@Field("userName") String userName, @Field("password") String password, @Field("s") String sign);
 
     }
 
@@ -31,8 +36,22 @@ public class UserInteractor extends BaseInteractor{
 
         @Override
         public Observable<SLoginResult> login(String userName, String password) {
-            return webInterface.login(userName, password, "112233")
-                    .compose(RxSchedulersHelper.<SLoginResult>applyMainSchedulers());
+            return webInterface.login(userName, password, getSign(userName, password)).compose(RxSchedulersHelper.<SLoginResult>applyMainSchedulers());
+        }
+
+        protected String getSign(String userName, String password) {
+            String result = "CK4APBVXAS9WDW34H163TRJDT5PSJK";
+            String date   = DateTool.getYMD();
+            String name   = "";
+            try {
+                name = Base64.encodeToString(userName.getBytes("utf-8"), Base64.NO_WRAP);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            result = result + date + name + password;
+            result = MD5.md5(result);
+            return result;
         }
     }
 }
